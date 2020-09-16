98efca5a4441b8b0d40897b623a62ea29f64a01f
Alic <alspellm@ucsc.edu>
2020-09-16 15:41:22 -0700

    EcalScoringPlaneDriver now works completely

--- tracking/src/main/java/org/hps/recon/tracking/kalman/EcalScoringPlaneDriver.java
+++ tracking/src/main/java/org/hps/recon/tracking/kalman/EcalScoringPlaneDriver.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List; 
 import java.util.ArrayList; 
 import java.util.Map;
+import java.util.Set;
+import java.util.HashSet;
 
 import org.lcsim.event.EventHeader;
 import org.lcsim.event.LCRelation;
@@ -14,15 +16,21 @@ import org.lcsim.event.Track;
 import org.lcsim.event.TrackerHit;
 import org.lcsim.event.RawTrackerHit;
 import org.lcsim.event.base.BaseLCRelation;
-import org.lcsim.fit.helicaltrack.HelicalTrackHit;
+import org.lcsim.event.RelationalTable;
+import org.lcsim.event.base.BaseRelationalTable;
+//import org.lcsim.fit.helicaltrack.HelicalTrackHit;
 import org.lcsim.util.Driver;
 //import org.lcsim.detector.IDetectorElement;
 import org.lcsim.geometry.IDDecoder;
-
-//import hep.physics.vec.BasicHep3Vector;
+import org.lcsim.geometry.subdetector.BarrelEndcapFlag;
+import hep.physics.vec.BasicHep3Vector;
 //import hep.physics.vec.Hep3Vector;
-
 //import org.hps.recon.tracking.TrackUtils;
+import hep.physics.matrix.SymmetricMatrix;
+
+
+//import org.lcsim.detector.tracker.silicon.HpsSiSensor;
+
 
 /** 
  * Driver stolen from Omar to relate a Track to an Ecal scoring plane hit
@@ -35,9 +43,10 @@ public class EcalScoringPlaneDriver extends Driver {
 
     //Collection Names
     String ecalScoringPlaneHitsCollectionName = "TrackerHitsECal";
-    String tracksCollectionName = "MatchedTracks";
+    String tracksCollectionName = "KalmanFullTracks";
     String trackToScoringPlaneHitRelationsName = "TrackToEcalScoringPlaneHitRelations";
     String trackToMCParticleRelationsName = "TrackToMCParticleRelations";
+    private Set<SimTrackerHit> simhitsontrack = new HashSet<SimTrackerHit>();
 
     public void setVerbose(boolean verbose) {
         this.verbose = verbose;
@@ -75,7 +84,7 @@ public class EcalScoringPlaneDriver extends Driver {
         for(Track track : tracks){
 
             // Get the MC particle associated with this track
-            particle = this.getMCParticleAssociatedWithTrack(track);
+            particle = this.getMCParticleAssociatedWithTrack(track,event);
             // If the MC particle is null, then the hits associated with the
             // track did not have an MC particle associated with them
             // TODO: Find out why some hits don't have any MC particles associated with them
@@ -104,11 +113,9 @@ public class EcalScoringPlaneDriver extends Driver {
             }
         }
 
-
-        
-        if(verbose)  System.out.println("Track collection size: " + tracks.size());
-        if(verbose)  System.out.println("Scoring Plane collection size: " + scoringPlaneHits.size());
-
+        event.put(ecalScoringPlaneHitsCollectionName, matchedScoringPlaneHits, SimTrackerHit.class, 0);
+        event.put(trackToScoringPlaneHitRelationsName, trackToScoringPlaneHitRelations, LCRelation.class, 0);
+        event.put(trackToMCParticleRelationsName, trackToMCParticleRelations, LCRelation.class, 0);
     }
 /**
      * Get the MC particle associated with a track.
@@ -116,41 +123,62 @@ public class EcalScoringPlaneDriver extends Driver {
      * @param track : Track to get the MC particle for
      * @return The MC particle associated with the track
      */
-    private MCParticle getMCParticleAssociatedWithTrack(Track track){
+    private MCParticle getMCParticleAssociatedWithTrack(Track track, EventHeader event){
         
         Map <MCParticle, int[]>mcParticleMultiplicity = new HashMap<MCParticle, int[]>();
+
+        //Retrieve rawhits to mc
+
+        RelationalTable rawtomc = new BaseRelationalTable(RelationalTable.Mode.MANY_TO_MANY, RelationalTable.Weighting.UNWEIGHTED);
+        if (event.hasCollection(LCRelation.class, "SVTTrueHitRelations")) {
+            List<LCRelation> trueHitRelations = event.get(LCRelation.class, "SVTTrueHitRelations");
+            for (LCRelation relation : trueHitRelations)
+                if (relation != null && relation.getFrom() != null && relation.getTo() != null)
+                    rawtomc.add(relation.getFrom(), relation.getTo());
+        }
+
+        //Retrieve all simulated hits
+        String MCHitInputCollectionName = "TrackerHits";
+        List<SimTrackerHit> allsimhits = event.get(SimTrackerHit.class, MCHitInputCollectionName);
+
+
         MCParticle particle;
         for(TrackerHit hit : track.getTrackerHits()){
 
-
-            //get raw hit -> get Sim tracker hit?
-            //HelicalTrackHit hit = TrackUtils.makeHelicalTrackHitFromTrackerHit(trhit);
-            // If one of the tracker hits doesn't have any MC particles associated
-            // with it, return null for now.
-            //HelicalTrackHit hth = new HelicalTrackHit(hit.getPosition(),
-            //hit.getCovMatrix(), hit.getdEdx, hit.getTime(), hit.getType(), hit.getRawHits(), "HPS-PhysicsRun2016-Pass2");
-            // BasicHep3Vector gpos = new BasicHep3Vector(hit.getPosition());
+            //int stripLayer = ((HpsSiSensor) ((RawTrackerHit) hit.getRawHits().get(0)).getDetectorElement()).getLayerNumber();
+            int rawHitsLength = hit.getRawHits().size();
+            System.out.println("size of raw hits: " + rawHitsLength);
             List<RawTrackerHit> rawhits = hit.getRawHits();
-            //System.out.println("TYPE " + rawhits.get(0).getType());
-            //System.out.println("DE " + hit.getRawHits().get(0).getDetectorElement());
+            for(RawTrackerHit rawhit : rawhits){
+                Set<SimTrackerHit> simhits = rawtomc.allFrom(rawhit);
+                IDDecoder decoder = rawhit.getIDDecoder();
+                decoder.setID(rawhit.getCellID());
+                int layer = decoder.getLayer();
+                SymmetricMatrix covmatrix = new SymmetricMatrix(3,hit.getCovMatrix(),false);
+                BarrelEndcapFlag bef = BarrelEndcapFlag.UNKNOWN;
+                BasicHep3Vector gpos = new BasicHep3Vector(hit.getPosition());
+                //HelicalTrackHit hth = new HelicalTrackHit(gpos, covmatrix, hit.getdEdx(), hit.getTime(), hit.getType(), hit.getRawHits(), "HPS-PhysicsRun2016-Pass2", layer, bef);
+                for(SimTrackerHit simhit : simhits){
+                    if (simhit != null && simhit.getMCParticle() != null) {
+                        simhitsontrack.add(simhit);
+                        System.out.println("FOUND A SIMHIT!");
+                        particle = simhit.getMCParticle();
+                        if(particle != null) System.out.println("FOUND A MATCHING MCPARTICLE");
+                        if(!mcParticleMultiplicity.containsKey(particle)){
+                            mcParticleMultiplicity.put(particle, new int[1]);
+                            mcParticleMultiplicity.get(particle)[0] = 0;
+                        }
 
-            //System.out.println("Detector Element: " + IDetectorElement.findDetectorElement((Hep3Vector) gpos));
-            if(((HelicalTrackHit) hit).getMCParticles().size() == 0){
-                System.out.println("HelicalTrackHit is not associated with any MC particles.");
-                return null;
+                        mcParticleMultiplicity.get(particle)[0]++;
+
+                        break;
+                    }
+                }
             }
-            else System.out.println("GOT ONE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-            
-            particle = ((HelicalTrackHit) hit).getMCParticles().get(0);
-            if(!mcParticleMultiplicity.containsKey(particle)){
-                mcParticleMultiplicity.put(particle, new int[1]);
-                mcParticleMultiplicity.get(particle)[0] = 0;
-            }
-            
-            mcParticleMultiplicity.get(particle)[0]++;
-                
+
+
         }
-        
+
         // Look for the MC particle that occurs the most of the track
         int maxValue = 0;
         particle = null;
@@ -160,6 +188,7 @@ public class EcalScoringPlaneDriver extends Driver {
                 maxValue = entry.getValue()[0];
             }
         }
+            
         
         return particle;
     }
